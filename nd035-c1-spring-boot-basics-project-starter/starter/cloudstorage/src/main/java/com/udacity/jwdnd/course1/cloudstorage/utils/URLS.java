d7f7990660aba4ab69087b45a00071cd2de6ebd6
tasneem <tasneemipraheem@gmail.com@users.noreply.github.com>
2021-10-03 21:15:19 +0200

    add bad model - delete file

--- nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/utils/URLS.java
+++
@@ -1,8 +0,0 @@
-package com.udacity.jwdnd.course1.cloudstorage.utils;
-
-public class URLS {
-	public static String  LOGIN_END_POINT = "login";
-	public static String  SIGNUP_END_POINT = "signup";
-	public static String  LOGOUT_END_POINT = "logout";
-	public static String  HOME_END_POINT = "home";
-}
