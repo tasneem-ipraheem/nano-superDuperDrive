e31c555030cce1cdd5279d3eaef505ebecf9dc34
tasneem <tasneemipraheem@gmail.com@users.noreply.github.com>
2021-10-04 19:40:22 +0200

    Junit + selenium test for [login.signup]

--- nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -7,35 +7,67 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+//TODO [Junit - Read resources]
+/**
+ * 1. Setup the driver : 
+ * 		https://classroom.udacity.com/nanodegrees/nd035/parts/f318240c-0513-47b3-bf26-09db96482caf/modules/1370646e-400f-4685-b916-5bc8e03554ce/lessons/67b1a5b0-14a8-419b-b9d1-c5604f9cbcbe/concepts/a792032b-e19f-4b49-aa90-be0d796cc6f0
+ * 2. Chat example :
+ * 		https://github.com/udacity/nd035-c1-spring-boot-basics-examples/tree/master/udacity-jwdnd-c1-l5-final-review-solution-master
+ * 3. DOCs : 
+ * 		https://www.selenium.dev/documentation/guidelines/page_object_models/
+ * 		https://junit.org/junit5/docs/current/user-guide/#writing-tests-assertions
+ * 		https://junit.org/junit5/docs/current/user-guide/#writing-tests-annotations
+ * 		https://github.com/bonigarcia/webdrivermanager
+ * 		https://www.selenium.dev/documentation/webdriver/waits/
+ * */
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
 	@LocalServerPort
-	private int port;
+	public int port;
 
-	private WebDriver driver;
+	public static WebDriver driver;
+
+	public String baseURL;
 
 	@BeforeAll
-	static void beforeAll() {
+	public static void beforeAll() {
 		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+	}
+
+	@AfterAll
+	public static void afterAll() {
+		driver.quit();
+		driver = null;
 	}
 
 	@BeforeEach
 	public void beforeEach() {
-		this.driver = new ChromeDriver();
-	}
-
-	@AfterEach
-	public void afterEach() {
-		if (this.driver != null) {
-			driver.quit();
-		}
+		baseURL = baseURL = "http://localhost:" + port;
 	}
 
 	@Test
-	public void getLoginPage() {
-		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
+	public void testUserSignupLoginAndSubmitMessage() {
+		String username = "pzastoup";
+		String password = "whatabadpassword";
+
+
+		driver.get(baseURL + "/signup");
+
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup("Peter", "Zastoupil", username, password);
+
+		driver.get(baseURL + "/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username, password);
+
+
 	}
+
+	
 
 }
